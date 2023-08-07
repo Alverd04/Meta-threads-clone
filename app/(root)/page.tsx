ed16a28bc3a0a3011c406c2da28468e0a212b3b6
Alverd04 <albert.font.fernandez@gmail.com>
2023-08-07 16:51:52 +0200

    layout finished

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,8 +3,8 @@ import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <div>
-      <UserButton showName afterSignOutUrl="/" />
-    </div>
+    <>
+      <h1 className="head-text text-left">Home</h1>
+    </>
   );
 }
---
+++ components/shared/Rightside.tsx
@@ -0,0 +1,16 @@
+function Rightsidebar() {
+  return (
+    <section className="custom-scrollbar rightsidebar">
+      <div className="flex flex-1 flex-col justify-start">
+        <h3 className="text-heading-4-medium text-light-1">
+          Suggested communities
+        </h3>
+      </div>
+      <div className="flex flex-1 flex-col justify-start">
+        <h3 className="text-heading-4-medium text-light-1">Suggested Users</h3>
+      </div>
+    </section>
+  );
+}
+
+export default Rightsidebar;
